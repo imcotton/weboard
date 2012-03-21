c8c72820ba574113f500389cc9a2199273ad18c3
Cotton Hou <himcotton@gmail.com>
2012-03-21 21:08:43 +0800

    add lesscss

--- projects/prototype/build/build.sh
+++ projects/prototype/build/build.sh
@@ -7,6 +7,7 @@ mkdir -p $output
 cp -r $source/* $output/
 
 find -type f -name *.coffee | xargs coffee -c
+find -type f -name *.less | sed 's/less$//' | awk '{print "lessc " $1 "less " $1 "css" }' | sh
 
 #exit 0
 
