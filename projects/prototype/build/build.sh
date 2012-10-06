b4c583677f62414008ef1a52151e8614b5fc4a2a
Cotton Hou <himcotton@gmail.com>
2012-10-06 23:38:39 +0800

    use UglifyJS to compress JS files

--- projects/prototype/build/build.sh
+++ projects/prototype/build/build.sh
@@ -7,7 +7,12 @@ rm -rf $output
 mkdir -p $output
 cp -r $source/* $output/
 
-find . -type f -name *.coffee | xargs coffee -c
+for file in `find . -type f -name '*.coffee'`
+do
+    coffee -c $file > /dev/null 2>&1
+    echo $file | sed 's|coffee$|js|' | xargs uglifyjs --overwrite
+done
+
 find . -type f -name *.less | sed 's/less$//' | awk '{print "lessc " $1 "less " $1 "css" }' | sh
 
 if [[ "$1" = build ]]; then
