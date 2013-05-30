df1f592e01bea57206b7b269fe90f94de013f75c
Cotton Hou <himcotton@gmail.com>
2013-05-30 22:18:47 +0800

    no need compile anymore

--- projects/prototype/build/build.sh
+++ projects/prototype/build/build.sh
@@ -7,13 +7,14 @@ rm -rf $output
 mkdir -p $output
 cp -r $source/* $output/
 
-for file in `find . -type f -name '*.coffee'`
-do
-    coffee -c $file > /dev/null 2>&1
-    echo $file | sed 's/coffee$/js/' | xargs uglifyjs --overwrite
-done
 
-find . -type f -name *.less | sed 's/less$//' | awk '{print "lessc " $1 "less " $1 "css" }' | sh
+#for file in `find . -type f -name '*.coffee'`
+#do
+#    coffee -c $file > /dev/null 2>&1
+#    echo $file | sed 's/coffee$/js/' | xargs uglifyjs --overwrite
+#done
+
+#find . -type f -name *.less | sed 's/less$//' | awk '{print "lessc " $1 "less " $1 "css" }' | sh
 
 if [[ "$1" = build ]]; then
     return
